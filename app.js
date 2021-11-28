74f8b5e79e918ed2b31379094f1612e96e68d341
gafur-demirci <gafur.gs.68gmail.com>
2021-11-28 17:24:10 +0300

    db post added

--- app.js
+++ app.js
@@ -1,17 +1,55 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
 
-app.use(express.static('public'))
+// Connect Db
+mongoose.connect('mongodb://localhost:27017/pcat-db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
+// TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTES
+
+// index view render
 app.get('/', (req, res) => {
-//   res.send('Merhaba');
-    res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+    //   res.send('Merhaba');
+    // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+    const photos = Photo.find({});
+    res.render('index', {
+        photos
+    });
+});
+
+// about view render
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+// add view render
+app.get('/add', (req, res) => {
+    res.render('add');
+});
+
+// post
+app.post('/photos', async (req, res) => {
+    await Photo.create(req.body)
+    res.redirect('/');
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Sunucu ${port} portunda başlatıldı..`);
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
 });
