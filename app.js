e9642ef3e898f9599c7daa3e56e17b5150209965
gafur-demirci <gafur.gs.68gmail.com>
2021-11-28 19:10:43 +0300

    Updated for upload image

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileUpload');
 const ejs = require('ejs');
 const path = require('path');
 const Photo = require('./models/Photo');
+const fs = require('fs');
 
 const app = express();
 
@@ -19,16 +21,26 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 // ROUTES
 
 // index view render
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     //   res.send('Merhaba');
     // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-    const photos = Photo.find({});
+    const photos = await Photo.find({}).sort('-dateCreated');
     res.render('index', {
-        photos
+        photos,
+    });
+});
+
+// single photo page
+app.get('/photos/:id', async (req, res) => {
+    // console.log(req.params.id);
+    const photo = await Photo.findById(req.params.id);
+    res.render('photo', {
+        photo,
     });
 });
 
@@ -42,11 +54,35 @@ app.get('/add', (req, res) => {
     res.render('add');
 });
 
-// post
+// post (db ye nesne gönderir)
 app.post('/photos', async (req, res) => {
-    await Photo.create(req.body)
-    res.redirect('/');
+
+    const uploadDir = 'public/uploads';
+
+    if(!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir)
+    }
+
+    let uploadeImage = req.files.image
+    let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name
+
+    uploadeImage.mv(uploadPath, async () => {
+        await Photo.create({
+            ...req.body,
+            image : '/uploads/' + uploadeImage.name
+        })
+        res.redirect('/');
+    });
+
+    //console.log(req.files.image);
+    //await Photo.create(req.body)
+    //res.redirect('/');
+
 });
+
+
+
+
 
 const port = 3000;
 
